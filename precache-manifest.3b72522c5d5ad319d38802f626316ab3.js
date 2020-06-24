428b8c94cd65dc16059610dfbb4d43c33aff658d
MrMurray25 <65830293+MrMurray25@users.noreply.github.com>
2020-06-24 17:17:22 -0300

    Add files via upload

---
+++ precache-manifest.3b72522c5d5ad319d38802f626316ab3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4514ad63ab21214c1174c10aa4b2450",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b202dd61f5716b3d0a07",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f7188e79bfa1dc794f85",
+    "url": "/static/css/main.5cd8c1bd.chunk.css"
+  },
+  {
+    "revision": "b202dd61f5716b3d0a07",
+    "url": "/static/js/2.538a2956.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.538a2956.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7188e79bfa1dc794f85",
+    "url": "/static/js/main.d664b59b.chunk.js"
+  },
+  {
+    "revision": "caefe7d433da51e0f4a9",
+    "url": "/static/js/runtime-main.559ef17b.js"
+  }
+]);
